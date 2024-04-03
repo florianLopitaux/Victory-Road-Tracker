71c3fb6bb9aae9fa48ba6e8a418fd92427708e65
Florian Lopitaux <florian.lopitaux@gmail.com>
2024-04-03 10:01:23 +0200

    Initialize repository

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+/**
+@file     index.php
+@author   Florian Lopitaux
+@version  0.1
+@summary  EntryPoint of the Victory-Road-Tracker, loads everything and does the 'url routing'.
+
+-------------------------------------------------------------------------
+
+Copyright (C) 2024 Victory-Road-Tracker
+
+Use of this software is governed by the GNU Public License, version 3.
+
+Victory-Road-Tracker is free RESTFUL API: you can use it under the terms
+of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Victory-Road-Tracker is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MindShare-API. If not, see <http://www.gnu.org/licenses/>.
+
+This banner notice must not be removed.
+
+-------------------------------------------------------------------------
+ */
+
+// Loads the .ini file that contains the database identifiers
+$config = parse_ini_file('config.ini');
+
+// file doesn't found or not parsable, should not happen
+if (!$config) {
+    http_response_code(500);
+    echo "<h1>Server internal problem !</h1>";
+    die();
+}
